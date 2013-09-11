fdc4d436e031d8c486df70d1184b2a93a935419c
Branan Purvine-Riley <branan@puppetlabs.com>
2013-09-11 16:44:55 -0700

    Add dependencies and a skeleton of the API endpoints

--- project.clj
+++ project.clj
@@ -1,7 +1,15 @@
 (defproject mtgboard "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Leaderboard for Magic: The Gathering League Play"
+  :plugins [[lein-ring "0.8.5"]]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main mtgboard.core)
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]
+                 [ring/ring-json "0.2.0"]
+                 [ragtime "0.3.3"]
+                 [korma "0.3.0-RC5"]
+                 [postgresql "9.1-901.jdbc4"]]
+  :ring {:handler mtgboard.core/app
+         :init mtgboard.core/init
+         :port 5893})
--- src/mtgboard/core.clj
+++ src/mtgboard/core.clj
@@ -1,9 +1,20 @@
 (ns mtgboard.core
-  (:gen-class))
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+(defroutes v1-routes
+  (GET "/players" [] "Listing players, yo")
+  (GET "/players/:player" [player] "Listing a single player, yo")
+  (GET "/matches" [] "Listing matches, yo. We should add query params to this one!")
+  (GET "/matches/:match" [match] "Listing a single match, yo"))
+
+(defroutes app-routes
+  (context "/api/v1" [] v1-routes))
+
+(def app
+  (handler/site app-routes))
+
+(defn init
+  "I should be setting up a database connection"
+  []
+  (println "When I grow up, I'm going to make database connections just like my daddy!"))

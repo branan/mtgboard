b9111ec450d58c90ab8696c70d03f67b9834f41a
Branan Purvine-Riley <branan@puppetlabs.com>
2013-09-17 19:33:53 -0700

    Add player name combo list from database of players

--- src/mtgboard/core.clj
+++ src/mtgboard/core.clj
@@ -4,6 +4,7 @@
             [compojure.handler :as handler]
             [ring.util.response :refer [response redirect]]
             [ring.middleware.json :as json]
+            [ring.middleware.params :as params]
             [mtgboard.db :as db]))
 
 
@@ -30,27 +31,85 @@
   [player]
   (let [id (Integer/valueOf player)
         player (db/get-player id)
-        matches (map #(get-match (:id %)) (db/list-matches-for-player id))]
+        matches (map #(db/get-match (:id %)) (db/list-matches-for-player id))]
     (assoc player :matches matches)))
 
+(defn player-score
+  [id match]
+  (if (= id (:player1_id match))
+    (:player1_wins match)
+    (:player2_wins match)))
+
+(defn opponent-score
+  [id match]
+  (if (= id (:player1_id match))
+    (:player2_wins match)
+    (:player1_wins match)))
+
+(defn games-won
+  [id matches]
+  (reduce + 0 (map #(player-score id %) matches)))
+
+(defn games-lost
+  [id matches]
+  (reduce + 0 (map #(opponent-score id %) matches)))
+
+(defn winning-matches
+  [matches player]
+  (filter #(= (:player1_id %) player) matches))
+
+(defn get-player-score
+  [player]
+  (let [id (Integer/valueOf (:id player))
+        matches (map #(db/get-match (:id %)) (db/list-matches-for-player id))
+        total (count matches)
+        wins (count (winning-matches matches id))]
+    (-> player
+        (assoc :matches-won wins)
+        (assoc :matches-lost (- total wins))
+        (assoc :games-won (games-won id matches))
+        (assoc :games-lost (games-lost id matches))
+        (assoc :rating (if total
+                         (/ wins total)
+                         0)))))
+
+(defn show-leaderboard
+  []
+  (->> (db/list-players)
+       (map get-player-score)
+       (sort-by :rating)
+       (reverse)))
+
+(defn list-players
+  []
+  (sort-by :name (db/list-players)))
+
 (defroutes v1-routes
+  (GET "/leaderboard" []
+       (response (show-leaderboard)))
   (GET "/players" []
-       (response (db/list-players)))
-  (GET "/players/:player" [player]
-       (response (get-player player)))
-  (GET "/matches" []
-       (response (db/list-matches)))
-  (GET "/matches/:match" [match]
-       (response (get-match match)))
+       (response (list-players)))
+  ;; (GET "/players/:player" [player]
+  ;;      (response (get-player player)))
+  ;; (GET "/matches" []
+  ;;      (response (db/list-matches)))
+  ;; (GET "/matches/:match" [match]
+  ;;      (response (get-match match)))
   (POST "/players" [name]
         (response (db/create-player name)))
-  (POST "/matches" [player1_id player2_id player1_wins player2_wins]
-        (response (db/create-match player1_id player2_id player1_wins player2_wins))))
+  (POST "/matches" [winner loser winner-score loser-score]
+        (let [winner (Integer/valueOf winner)
+              loser (Integer/valueOf loser)
+              winner-score (Integer/valueOf winner-score)
+              loser-score (Integer/valueOf loser-score)]
+          (if (< winner-score loser-score)
+            (response "")
+            (response (db/create-match winner loser winner-score loser-score))))))
 
 (defroutes app-routes
   (GET "/" [] (redirect "/index.html"))
   (route/resources "/")
-  (context "/api/v1" [] (-> v1-routes json/wrap-json-response json/wrap-json-params)))
+  (context "/api/v1" [] (-> v1-routes params/wrap-params json/wrap-json-response json/wrap-json-params)))
 
 (def app
   (handler/site app-routes))
